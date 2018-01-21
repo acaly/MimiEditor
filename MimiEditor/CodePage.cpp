76e01ac231abcfc7b78a01980ec1247c06080163
Zhenwei Wu <wuzhenwei35@gmail.com>
2018-01-21 00:46:37 -0500

    Fix char type in CodePage to internal types.

--- MimiEditor/CodePage.cpp
+++ MimiEditor/CodePage.cpp
@@ -1,4 +1,4 @@
-#include "CodePages.h"
+#include "CodePage.h"
 #include <locale>
 
 static bool Initialized = false;
@@ -6,38 +6,40 @@ Mimi::CodePageManager Instance;
 
 namespace
 {
+	using Mimi::mchar8_t;
+
 	class UTF8Impl : public Mimi::CodePageImpl
 	{
 	private:
-		Mimi::BufferIncrement WriteUTF16(std::uint8_t r, std::uint32_t unicode, std::uint16_t* dest)
+		Mimi::BufferIncrement WriteUTF16(std::uint8_t r, char32_t unicode, char16_t* dest)
 		{
-			if (unicode < 0x10000)
+			if (unicode < 0x10000u)
 			{
-				dest[0] = unicode;
+				dest[0] = static_cast<char16_t>(unicode);
 				return { r, 1 };
 			}
 			else
 			{
-				unicode -= 0x10000;
-				dest[0] = 0xD800 + (unicode >> 10);
-				dest[1] = 0xDC00 + (unicode & 0x3FF);
+				unicode -= 0x10000u;
+				dest[0] = static_cast<char16_t>(0xD800u + (unicode >> 10));
+				dest[1] = static_cast<char16_t>(0xDC00u + (unicode & 0x3FF));
 				return { r, 2 };
 			}
 		}
 
-		int ReadUTF16(const std::uint16_t* src, std::uint32_t* unicode)
+		std::uint8_t ReadUTF16(const char16_t* src, char32_t* unicode)
 		{
-			if (src[0] >= 0xD800 && src[0] < 0xE000)
+			if (src[0] >= 0xD800u && src[0] < 0xE000u)
 			{
-				if (src[0] >= 0xDC00)
+				if (src[0] >= 0xDC00u)
 				{
 					return 0; //not lead surrogate
 				}
-				if (src[1] < 0xDC00 || src[1] >= 0xE000)
+				if (src[1] < 0xDC00u || src[1] >= 0xE000u)
 				{
 					return 0; //not trail surrogate
 				}
-				*unicode = 0x10000 + ((src[0] - 0xD800) << 10 | (src[1] - 0xDC00));
+				*unicode = 0x10000u + ((src[0] - 0xD800u) << 10 | (src[1] - 0xDC00u));
 				return 2;
 			}
 			else
@@ -53,9 +55,9 @@ namespace
 			return 1;
 		}
 
-		virtual Mimi::BufferIncrement CharToUTF16(const std::uint8_t* src, std::uint16_t* dest)
+		virtual Mimi::BufferIncrement CharToUTF16(const mchar8_t* src, char16_t* dest) override
 		{
-			if (src[0] < 0x80)
+			if (src[0] < 0x80u)
 			{
 				dest[0] = src[0];
 				return { 1, 1 };
@@ -85,38 +87,38 @@ namespace
 			}
 		}
 
-		virtual Mimi::BufferIncrement CharFromUTF16(const std::uint16_t* src, std::uint8_t* dest)
+		virtual Mimi::BufferIncrement CharFromUTF16(const char16_t* src, mchar8_t* dest) override
 		{
-			std::uint32_t unicode;
+			char32_t unicode;
 			std::uint8_t r = ReadUTF16(src, &unicode);
 
-			if (unicode < 0x80)
+			if (unicode < 0x80u)
 			{
-				dest[0] = static_cast<std::uint8_t>(unicode);
+				dest[0] = static_cast<mchar8_t>(unicode);
 				return { r, 1 };
 			}
-			else if (unicode < 0x800)
+			else if (unicode < 0x800u)
 			{
-				dest[0] = static_cast<std::uint8_t>(0xC0 | (unicode >> 6));
-				dest[1] = static_cast<std::uint8_t>(0x80 | (unicode & 0x3F));
+				dest[0] = static_cast<mchar8_t>(0xC0u | (unicode >> 6));
+				dest[1] = static_cast<mchar8_t>(0x80u | (unicode & 0x3Fu));
 				return { r, 2 };
 			}
-			else if (unicode < 0x10000)
+			else if (unicode < 0x10000u)
 			{
-				dest[0] = static_cast<std::uint8_t>(0xE0 | (unicode >> 12));
-				dest[1] = static_cast<std::uint8_t>(0x80 | ((unicode >> 6) & 0x3F));
-				dest[2] = static_cast<std::uint8_t>(0x80 | (unicode & 0x3F));
+				dest[0] = static_cast<mchar8_t>(0xE0u | (unicode >> 12));
+				dest[1] = static_cast<mchar8_t>(0x80u | ((unicode >> 6) & 0x3Fu));
+				dest[2] = static_cast<mchar8_t>(0x80u | (unicode & 0x3Fu));
 				return { r, 3 };
 			}
-			else if (unicode < 0x110000)
+			else if (unicode < 0x110000u)
 			{
-				dest[0] = static_cast<std::uint8_t>(0xF0 | (unicode >> 18));
-				dest[1] = static_cast<std::uint8_t>(0x80 | ((unicode >> 12) & 0x3F));
-				dest[2] = static_cast<std::uint8_t>(0x80 | ((unicode >> 6) & 0x3F));
-				dest[3] = static_cast<std::uint8_t>(0x80 | (unicode & 0x3F));
+				dest[0] = static_cast<mchar8_t>(0xF0u | (unicode >> 18));
+				dest[1] = static_cast<mchar8_t>(0x80u | ((unicode >> 12) & 0x3Fu));
+				dest[2] = static_cast<mchar8_t>(0x80u | ((unicode >> 6) & 0x3Fu));
+				dest[3] = static_cast<mchar8_t>(0x80u | (unicode & 0x3Fu));
 				return { r, 4 };
 			}
-			return { 0,0 };
+			return { 0, 0 };
 		}
 	};
 
@@ -128,13 +130,13 @@ namespace
 			return 2;
 		}
 
-		virtual Mimi::BufferIncrement CharToUTF16(const std::uint8_t* src, std::uint16_t* dest)
+		virtual Mimi::BufferIncrement CharToUTF16(const mchar8_t* src, char16_t* dest) override
 		{
-			auto d0 = *reinterpret_cast<const std::uint16_t*>(src);
-			if (d0 >= 0xD800 && d0 < 0xE000)
+			char16_t d0 = *reinterpret_cast<const char16_t*>(src);
+			if (d0 >= 0xD800u && d0 < 0xE000u)
 			{
 				dest[0] = d0;
-				dest[1] = *reinterpret_cast<const std::uint16_t*>(&src[2]);
+				dest[1] = *reinterpret_cast<const char16_t*>(&src[2]);
 				return { 4, 2 };
 			}
 			else
@@ -144,17 +146,17 @@ namespace
 			}
 		}
 
-		virtual Mimi::BufferIncrement CharFromUTF16(const std::uint16_t* src, std::uint8_t* dest)
+		virtual Mimi::BufferIncrement CharFromUTF16(const char16_t* src, mchar8_t* dest) override
 		{
-			if (src[0] >= 0xD800 && src[0] < 0xE000)
+			if (src[0] >= 0xD800u && src[0] < 0xE000u)
 			{
-				*reinterpret_cast<std::uint16_t*>(dest) = src[0];
-				*reinterpret_cast<std::uint16_t*>(&dest[2]) = src[1];
+				*reinterpret_cast<char16_t*>(dest) = src[0];
+				*reinterpret_cast<char16_t*>(&dest[2]) = src[1];
 				return { 2, 4 };
 			}
 			else
 			{
-				*reinterpret_cast<std::uint16_t*>(dest) = src[0];
+				*reinterpret_cast<char16_t*>(dest) = src[0];
 				return { 1, 2 };
 			}
 		}
--- MimiEditor/CodePage.h
+++ MimiEditor/CodePage.h
@@ -3,6 +3,8 @@
 
 namespace Mimi
 {
+	typedef std::uint8_t mchar8_t; //I hate C++
+
 	struct BufferIncrement
 	{
 		std::uint8_t Source;
@@ -17,12 +19,12 @@ namespace Mimi
 		//Convert a character from the code page to utf16.
 		//src: char data. This function may read a maximum of 4 bytes from here.
 		//dest: buffer. This function may write the first 2 or 4 bytes to here.
-		virtual BufferIncrement CharToUTF16(const std::uint8_t* src, std::uint16_t* dest) = 0;
+		virtual BufferIncrement CharToUTF16(const mchar8_t* src, char16_t* dest) = 0;
 		
 		//Convert a character from utf16 to the code page.
 		//src: utf16 char data. This function may read the first 2 or 4 bytes from here.
 		//dest: char data. This function may write a maximum of 4 bytes to here.
-		virtual BufferIncrement CharFromUTF16(const std::uint16_t* src, std::uint8_t* dest) = 0;
+		virtual BufferIncrement CharFromUTF16(const char16_t* src, mchar8_t* dest) = 0;
 	};
 
 	class CodePageManager;
@@ -36,12 +38,12 @@ namespace Mimi
 	public:
 		int NormalWidth() { return Impl->NormalWidth(); }
 
-		BufferIncrement CharToUTF16(const std::uint8_t* src, std::uint16_t* dest)
+		BufferIncrement CharToUTF16(const mchar8_t* src, char16_t* dest)
 		{
 			return Impl->CharToUTF16(src, dest);
 		}
 
-		BufferIncrement CharFromUTF16(const std::uint16_t* src, std::uint8_t* dest)
+		BufferIncrement CharFromUTF16(const char16_t* src, mchar8_t* dest)
 		{
 			return Impl->CharFromUTF16(src, dest);
 		}
