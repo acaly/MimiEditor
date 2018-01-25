6d60f8a206773c7816e493225bc6cf87fb2180de
Zhenwei Wu <wuzhenwei35@gmail.com>
2018-01-25 01:00:42 -0500

    Add FileTypeDetector API, no implementation.

--- MimiEditor/FileTypeDetector.h
+++ MimiEditor/FileTypeDetector.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <cstddef>
 #include <vector>
+#include <memory>
+#include <cassert>
 #include "CodePage.h"
+#include "IFile.h"
+#include "Buffer.h"
 
 namespace Mimi
 {
@@ -12,16 +16,79 @@ namespace Mimi
 		std::vector<CodePage> AdditionalCodePages;
 	};
 
+	enum class TextFileEncoding
+	{
+		Unknown,
+		UTF8,
+		UTF16LE,
+		UTF16BE,
+		CodePage,
+	};
+
 	class FileTypeDetector final
 	{
 	public:
-		FileTypeDetector();
+		FileTypeDetector(std::unique_ptr<IFileReader> reader, FileTypeDetectionOptions options);
 		FileTypeDetector(const FileTypeDetector&) = delete;
 		FileTypeDetector(FileTypeDetector&&) = delete;
 		FileTypeDetector& operator= (const FileTypeDetector&) = delete;
 		~FileTypeDetector();
 
-	public:
+	private:
+		bool IsText;
+		std::unique_ptr<IFileReader> TextContent;
+		DynamicBuffer InternalBuffer;
+		std::size_t LineIndex;
+		std::size_t InternalBufferPointer;
+		bool Continuous;
+		bool Unfinished;
+		bool EncodingDeceided;
+		bool CodePageDecided;
+		TextFileEncoding TextEncoding;
+		CodePage TextCodePage;
 
+	public:
+		bool IsTextFile()
+		{
+			return IsText;
+		}
+
+		TextFileEncoding GetEncoding()
+		{
+			assert(IsTextFile());
+			return TextEncoding;
+		}
+
+		CodePage GetCodePage()
+		{
+			assert(IsTextFile());
+			return TextCodePage;
+		}
+
+	public:
+		std::size_t GetCurrentLineIndex()
+		{
+			assert(IsTextFile());
+			return LineIndex;
+		}
+
+		DynamicBuffer CurrentLineData;
+
+		bool IsCurrentLineContinuous()
+		{
+			assert(IsTextFile());
+			return Continuous;
+		}
+
+		bool IsCurrentLineUnfinished()
+		{
+			assert(IsTextFile());
+			return Unfinished;
+		}
+
+		void ReadNextLine();
+		
+	public:
+		const std::unique_ptr<IFileReader> BinaryContent;
 	};
 }
